02cbac55f12befd811688ddb14ce028d1e9dcb34
Daisuke Murase <typester@cpan.org>
2010-01-29 14:25:45 +0900

    prototype

--- lib/Plack/App/Reproxy.pm
+++ lib/Plack/App/Reproxy.pm
@@ -1,8 +1,107 @@
 package Plack::App::Reproxy;
 use strict;
 use warnings;
+use parent 'Plack::Component';
+use Plack::Util::Accessor qw/backend/;
 
 our $VERSION = '0.01';
+
+use Plack::Request;
+use AnyEvent;
+use AnyEvent::HTTP;
+
+sub call {
+    my ($self, $env) = @_;
+
+    my $req = Plack::Request->new($env);
+
+    my $res = AE::cv;
+
+    my $uri     = URI->new( $self->backend . $req->uri->path_query );
+    my %headers = map { $_ => $req->header($_) } $req->headers->header_field_names;
+
+    my $proxy_method  = $req->method;
+    my $proxy_uri     = $uri;
+    my $proxy_headers = \%headers;
+    my $proxy_content = $req->raw_body;
+    my $proxy_callback;
+
+    my $proxy; $proxy = sub {
+        http_request(
+            $proxy_method => $proxy_uri,
+            headers       => $proxy_headers,
+            $proxy_content ? (body => $proxy_content) : (),
+            sub {
+                my ($body, $hdr) = @_;
+
+                my $status = delete $hdr->{Status};
+
+                if ($status =~ /^59/) {
+                    # internal error
+                    warn "Proxy error: $status $hdr->{Reason}, $hdr->{URL}";
+                    $res->send([
+                        500,
+                        [ 'Content-Length' => 21, 'Content-Type' => 'text/plain' ],
+                        ['Internal Server Error'],
+                    ]);
+                }
+                else {
+                    # remove pseudo-headers
+                    delete @$hdr{qw/HTTPVersion Reason URL/};
+
+                    if ($hdr->{'x-reproxy-url'}) {
+                        # reproxy
+                        $proxy_method = $hdr->{'x-reproxy-method'} || 'GET';
+                        $proxy_uri    = $hdr->{'x-reproxy-url'};
+                        $proxy_headers = {};
+                        for my $h (grep /^x-reproxy-header-/, keys %$hdr) {
+                            $proxy_headers->{$h} = $hdr->{$h};
+                        }
+                        $proxy_content  = $body;
+                        $proxy_callback = $hdr->{'x-reproxy-callback'};
+
+                        my $t; $t = AnyEvent->timer(
+                            after => 0,
+                            cb    => sub {
+                                undef $t;
+                                $proxy->();
+                            },
+                        );
+                    }
+                    elsif ($proxy_callback) {
+                        $proxy_method = 'POST';
+                        $proxy_uri    = $proxy_callback;
+                        $proxy_headers = $hdr;
+                        $proxy_content = $body;
+                        undef $proxy_callback;
+
+                        my $t; $t = AnyEvent->timer(
+                            after => 0,
+                            cb    => sub {
+                                undef $t;
+                                $proxy->();
+                            },
+                        );
+                    }
+                    else {
+                        $res->send([
+                            $status,
+                            [%$hdr],
+                            [$body || ''],
+                        ]);
+                    }
+                }
+            },
+        );
+    };
+    $proxy->();
+
+    $res;
+}
+
+1;
+
+__END__
 
 =head1 NAME
 
@@ -36,5 +135,3 @@ The full text of the license can be found in the
 LICENSE file included with this module.
 
 =cut
-
-1;
